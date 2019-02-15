995b5471ee85715ba1ef7867b568025014aa36ea
RolandDeschainRPW <raffaele.plmr@gmail.com>
2019-02-15 16:42:38 +0100

    IA nemici, suddivisione scene, caricamento dinamico livelli

---
+++ VR Project 2018/Assets/Scenes/GameScene/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static GameManagerScript Instance{ set; get; }
+
+    private void Awake()
+    {
+        Instance = this;
+        Load("Player");
+        Load("Level01");
+        //Load("Level02");
+    }
+
+    public void Load(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void Unload(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.UnloadScene(sceneName);
+    }
+}
---
+++ VR Project 2018/Assets/Scenes/Levels/LoadTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadTrigger : MonoBehaviour
+{
+    public string loadName;
+    public string unloadName;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (loadName != "")
+            GameManagerScript.Instance.Load(loadName);
+
+        if (unloadName != "")
+            StartCoroutine("UnloadScene");
+    }
+
+    IEnumerator UnloadScene()
+    {
+        yield return new WaitForSeconds(.10f);
+        GameManagerScript.Instance.Unload(unloadName);
+    }
+}
