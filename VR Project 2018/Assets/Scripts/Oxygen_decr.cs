ff3ea0c214d1f6ab31c90f8b20068d97979aa50f
dax1994 <47638031+dax1994@users.noreply.github.com>
2019-02-20 11:52:23 +0100

    slider hp e ossigeno

---
+++ VR Project 2018/Assets/Scripts/Oxygen_decr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Oxygen_decr : MonoBehaviour
+{
+    public Slider ossigeno;
+    public float Ox;
+    public float MAXOX;// 1000
+    void Start()
+    {
+        Ox = MAXOX;
+        ossigeno = GetComponent<Slider>();
+        ossigeno.maxValue = MAXOX;
+        ossigeno.value = Ox;
+    }
+    void Update()
+    {
+        Ox -= 0.01f;
+        ossigeno.value = Ox;
+    }
+}
