d3eb1dc0c8a8ec1244506500fd7f6f4911a4e4fa
RolandDeschainRPW <raffaele.plmr@gmail.com>
2019-02-23 19:57:44 +0100

    fix ombre

--- VR Project 2018/Assets/Scripts/MyGameManagerScript.cs
+++ VR Project 2018/Assets/Scripts/MyGameManagerScript.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class MyGameManagerScript : MonoBehaviour
 {
     public static MyGameManagerScript Instance{ set; get; }
-    public Vector3 lastCheckPointPos;
-    public int checkPointNum;
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    */
     private void Awake()
     {
         if (Instance == null) {
